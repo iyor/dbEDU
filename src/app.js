d3763c556df0929767043242d95949b6b78c044c
hgpc <herman.carlstrom@gmail.com>
2017-02-27 13:13:29 +0100

    Now a better file structure, with working import

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import config from 'config'
-import db from 'db'
+//import db from 'db'
 
 require('dotenv').config()
 
--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ import {} from 'dotenv/config'
 //const PORT = process.env.PORT || '3000'
 const DB_URL = process.env.DB_URL;
 
-export default {
-//  PORT,
+export default  {
   DB_URL
 }

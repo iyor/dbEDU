28aa7034cd7c4b98d5989fb9cd55afbf06a06647
Filip Larsson <filip.larsson.vbg@gmail.com>
2017-03-07 11:46:36 +0100

    changed a name

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import {} from 'dotenv/config'
 
 const PORT = process.env.PORT || '3000'
 const DB_URL = process.env.DB_URL
-const DB_COLL = 'documents'
+const DB_COLL = 'Courses'
 export default  {
   DB_URL,
   PORT,

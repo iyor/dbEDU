8864f25c86c3aacb89c761f08ee067db0dbf7082
Herman <hgpc@users.noreply.github.com>
2017-03-22 16:17:27 +0100

    Updated the student/admin routes, as well as made changes to get it working with remote server(heroku). (#12)
    * Removed old files and changed functions for users in the DB.
    
    * Changed to add default value for the config file to support heroku.
    
    * updated npm start to function with heroku. hopefully.
    
    * Additional changes for heroku..
    
    * another try for heroku
    
    * Another try for the heroku..
    
    * heroku
    
    * heroku again
    
    * think this is the last
    '

--- src/routes/adminroutes.js
+++ src/routes/adminroutes.js
@@ -1,6 +1,19 @@
 import config from 'config'
 import { dbClient } from 'services/db'
 
+export function listCollections() {
+  let allCols = dbClient.listCollections()
+  return allCols.toArray()
+}
+
+
+export function dropAll() {
+  let remCol = dbClient.collection(config.DB_COLL)
+  return remCol.drop().catch((error) => {
+    return 'This action is not allowed'
+  })
+}
+
 export function removeOne (courseName) {
   let courses = dbClient.collection(config.DB_COLL)
   return courses.findOneAndDelete(
@@ -12,35 +25,13 @@ export function removeOne (courseName) {
     return 'This action is not allowed'
   })
 }
-
-export function testFind () {
-  let test = dbClient.collection(config.DB_COLL)
-  return test.find({a:1}).toArray().then(function(returnObj) {
-    return returnObj
-  })
-}
-
-export function dropAll() {
-  let remCol = dbClient.collection(config.DB_COLL)
-  return remCol.drop().catch((error) => {
+export function addCourse(courseName, courseDesc, courseMaterial) {
+  let courseList = dbClient.collection(config.DB_COLL)
+  return courseList.insert({
+    Course_Name : courseName,
+    Course_Descr : courseDesc,
+    Course_Material : courseMaterial
+  }).catch((error) => {
     return 'This action is not allowed'
   })
 }
-
-export function listCollections() {
-  let allCols = dbClient.listCollections()
-  return allCols.toArray()
-}
-
-export function addCourse(courseName, courseDesc, courseMaterial) {
-  let courseList = dbClient.collection(config.DB_COLL)
-    return courseList.insert({
-      Course_Name : courseName,
-      Course_Descr : courseDesc,
-      Course_Material : courseMaterial
-    }).catch((error) => {
-      return 'This action is not allowed'
-    })
-}
-
-    

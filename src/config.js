0501f6036886fc76d7d2641e926f736dbdf238b8
hgpc <herman.carlstrom@gmail.com>
2017-03-22 14:47:51 +0100

    another try for heroku

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import {} from 'dotenv/config'
 
 const PORT = process.env.PORT || '3000'
 //const DB_URL = process.env.DB_URL  // temporarily disabled to check reader only
-const DB_URL = process.env.DB_URL_TEST || DB_URL//temporarily added to check reader only user
+const DB_URL = process.env.DB_URL_TEST //temporarily added to check reader only user
 const DB_COLL = 'Courses'
 export default  {
   DB_URL,

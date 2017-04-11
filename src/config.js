fded4ed8b5e93d2d4eab1402a98102d73426a2a1
Erik <erikejan@hotmail.com>
2017-04-11 16:39:11 +0200

    Fixed newline as per hgp:s request

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || '3000'
 const DB_URL = process.env.DB_URL_TEST //temporarily added to check reader only user
 const DB_COURSES = 'Courses'
 const DB_EVALS = 'Evaluators'
+
 export default  {
   DB_URL,
   PORT,
